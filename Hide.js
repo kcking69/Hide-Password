3041fbacf1246546d5273fa55321727213154341
Nkemdilim Collins <collinsnkemdilim05@gmail.com>
2024-07-01 21:05:12 +0100

    first commit

---
+++ Hide.js
@@ -0,0 +1,67 @@
+const password = document.getElementById('password')
+const reveal = document.getElementById('reveal')
+
+reveal.addEventListener('click', ()=>{
+    if (password.type == 'password') {
+        password.type = 'text'
+    }else{
+        password.type = 'password'
+    }
+        
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
